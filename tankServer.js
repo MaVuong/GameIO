ff336b958000b6cbcfbd93cc8d969adfbb8e34a1
giangngo <gamesvn16@gmail.com>
2016-07-10 01:19:53 +0700

    ip config

--- tankServer.js
+++ tankServer.js
@@ -13,25 +13,18 @@ app.use(express.static(__dirname + '/DataGame'));
 
 app.get('/config', function(req, res){
   
-  var objectsend={};
+   var objectsend={};
   objectsend.display_banner=1;
   objectsend.display_fullscreen=1;
   objectsend.port=2020;
-  objectsend.timezone=timezone;
-  objectsend.ucode="US";
-
   objectsend.gameip="104.197.35.76";//ip server dat tai my
-
-  if (typeof(req.query.local)!== 'undefined') {
-  		var timezone=Number(req.query.local);
-
-	  if (timezone>=(-2)&&timezone<=4) {
-	  	objectsend.gameip="104.197.35.76";//ip server dat tai EU:  104.155.45.73
-	  	objectsend.ucode="EU";
-	  }
-	  if (timezone>4) {
-	  	objectsend.gameip="104.197.35.76";//ip server o chau A   : 104.199.172.133
-	  	objectsend.ucode="ASIA";
+  
+  if (req.query.local !== 'undefined') {
+  		var timezone = Number(req.query.local);
+	  if (timezone >= (-2) && timezone <= 4) {
+	  	objectsend.gameip="104.155.45.73";//ip server dat tai EU:  104.155.45.73
+	  } else if (timezone > 4) {
+	  	objectsend.gameip="104.199.172.133";//ip server o chau A   : 104.199.172.133
 	  }			
   }
   
